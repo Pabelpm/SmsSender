44cb90a59df33a2b7d1caaa775ebe1466f1ec581
Pablo Prieto Martin <pabelpm@gmail.com>
2024-11-19 15:28:06 +0100

    Space added

--- app/src/main/java/com/pabloprieto/smssender/MainActivity.kt
+++ app/src/main/java/com/pabloprieto/smssender/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SmsSenderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android ",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
