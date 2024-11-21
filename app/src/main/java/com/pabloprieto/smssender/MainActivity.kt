e4ca9d017e4cb96abac74bc4b5f71194bbdf0947
Pablo Prieto Martin <pabelpm@gmail.com>
2024-11-21 11:18:21 +0100

    Get phone numbers and send sms button

--- app/src/main/java/com/pabloprieto/smssender/MainActivity.kt
+++ app/src/main/java/com/pabloprieto/smssender/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text as TextM
@@ -26,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.pabloprieto.smssender.ui.theme.SmsSenderTheme
 
@@ -62,6 +64,14 @@ class MainActivity : ComponentActivity() {
                             TextM(text = "Request permission")
                         }
                         TextM(text = permissionStatus)
+
+                        Button(
+                            modifier = Modifier.padding(top= 32.dp),
+                            onClick = {                             },
+                            enabled = granted
+                        ) {
+                            TextM(text = "Get all phone numbers and send sms")
+                        }
                     }
                 }
             }
